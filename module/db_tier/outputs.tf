1fc37c4e2d2bb25295178c894dd367e578a5284c
manibrar <manvirbrar1988@gmail.com>
2018-08-08 23:07:36 +0100

    homework

---
+++ module/db_tier/outputs.tf
@@ -0,0 +1,10 @@
+output "app_vpc_id" {
+description = "vpc of app"
+  value = "${aws_vpc.app.id}"
+
+}
+
+output "db_ami" {
+   description = "foo"
+   value ="ami-01020378"
+}
---
+++ module/db_tier/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+  description = "the name of the resource"
+}
+
+variable "name" {
+  description = "the name of the resource"
+}
+
+variable "db_ami_id" {
+  description = "dbami"
+}
+
+variable "cidr_block" {
+  description = "cidr"
+}
