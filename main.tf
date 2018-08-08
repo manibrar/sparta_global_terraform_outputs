395a6d592a129d9154366e0ccc271b79c95111b5
manibrar <manvirbrar1988@gmail.com>
2018-08-08 16:48:38 +0100

    output ami

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "app_tier" {
   source = "./module/app_tier"
   vpc_id = "${module.app_tier.vpc_id}"
   name ="app-markson"
-  app_ami_id ="ami-c2b8bfbb"
-  db_ami_id ="ami-01020378"
+  app_ami_id ="${module.app_tier.app_ami}"
+  db_ami_id ="${module.app_tier.db_ami}"
   cidr_block ="10.0.0.0/16"
 }
--- module/app_tier/outputs.tf
+++ module/app_tier/outputs.tf
@@ -3,3 +3,13 @@ description = "vpc of app"
   value = "${aws_vpc.app.id}"
 
 }
+
+output "app_ami" {
+   description = "foo"
+   value ="ami-c2b8bfbb"
+}
+
+output "db_ami" {
+   description = "foo"
+   value ="ami-01020378"
+}
